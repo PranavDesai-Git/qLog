3d7247690feb33392146ecfa5c70926ec8be7b1a
Pranav Desai <contact.pranavdesai@gmail.com>
2026-05-22 11:53:53 +0530

    feat: bootstrap app entry point

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/PranavDesai-Git/qLog/ui"
+	"github.com/charmbracelet/bubbletea"
+)
+
+func main() {
+	m := ui.New()
+	p := tea.NewProgram(
+		m,
+		tea.WithAltScreen(),
+	)
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("err:%s", err)
+		os.Exit(1)
+	}
+}
